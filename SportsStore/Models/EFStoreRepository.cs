1f71f6b51be18049f4cf52b1f32c055a6405abf3
chrisalz <chrisalz123@gmail.com>
2024-05-18 23:59:20 +0800

    Add project files.

---
+++ SportsStore/Models/EFStoreRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace SportsStore.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        private StoreDBContext context;
+
+        public EFStoreRepository(StoreDBContext context)
+        {
+            this.context = context;
+        }
+        public IQueryable<Product> Products => context.Products;
+    }
+}
